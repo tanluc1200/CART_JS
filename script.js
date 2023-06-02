6a0dbc5c05c52f5224d77c0a48e96f8fa8845c7b
tanluc1200 <121601253+tanluc1200@users.noreply.github.com>
2023-06-02 10:45:04 +0700

    3 files modified

--- script.js
+++ script.js
@@ -45,6 +45,7 @@ const products = [
 console.log(products);
 const header_main = document.querySelector('.header-main');
 const listCarts = document.querySelector('.listCart');
+const total = document.querySelector('.total');
 function loadItemProduct() {
   products.forEach((product, key) => {
     const newDiv = document.createElement('div');
@@ -66,38 +67,87 @@ function loadItemProduct() {
   });
 }
 let listCart = [];
-function addToCart(key) {
-  if (listCart[key] === null) {
-    listCart[key] = products[key];
-    listCart[key].quantity = 1;
+function addToCart(index) {
+  if (listCart[index] == null) {
+    listCart[index] = {
+      ...products[index],
+      quantity: 1,
+    };
   }
-  reloadCart();
+  // else {
+  //   listCart[index].quantity++;
+  // }
+  renderUI();
 }
-function reloadCart() {
+function renderUI() {
+  // Tải lại danh sách giỏ hàng
   listCarts.innerHTML = '';
-  let quantity = 0;
-  let totalPrice = 0;
-  listCart.forEach((value, key) => {
-    totalPrice += value.price;
-    quantity += value.quantity;
-    if (value != null) {
-      console.log(value);
-      let newDiv = document.createElement('li');
-      newDiv.innerHTML = `
-          <div><img src="image/${value.image}"/></div>
-          <div>${value.name}</div>
-          <div>${value.price.toLocaleString()}</div>
-          <div>
-              <button onclick="changeQuantity(${key}, ${
-        value.quantity - 1
+  totalPrice = 0;
+  count = 0;
+  listCart.forEach((card, index) => {
+    if (card != null) {
+      totalPrice += card.price;
+      count += card.quantity;
+      const newLi = document.createElement('li');
+      newLi.classList.add('cart-item');
+      newLi.innerHTML = `
+        <div><img src="${card.image}"/></div>
+        <div>${card.name}</div>
+        <div>${card.price.toLocaleString()} VND</div>
+        <div class="cart-quantity">
+          <button onclick="changeQuantity(${index}, ${
+        card.quantity - 1
       })">-</button>
-              <div class="count">${value.quantity}</div>
-              <button onclick="changeQuantity(${key}, ${
-        value.quantity + 1
+          <div class="count">${card.quantity}</div>
+          <button onclick="changeQuantity(${index}, ${
+        card.quantity + 1
       })">+</button>
-          </div>`;
-      listCarts.appendChild(newDiv);
+      <svg onClick="deleteCartItem(${index})" fill="currentColor" class="cart-delete" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M170.5 51.6L151.5 80h145l-19-28.4c-1.5-2.2-4-3.6-6.7-3.6H177.1c-2.7 0-5.2 1.3-6.7 3.6zm147-26.6L354.2 80H368h48 8c13.3 0 24 10.7 24 24s-10.7 24-24 24h-8V432c0 44.2-35.8 80-80 80H112c-44.2 0-80-35.8-80-80V128H24c-13.3 0-24-10.7-24-24S10.7 80 24 80h8H80 93.8l36.7-55.1C140.9 9.4 158.4 0 177.1 0h93.7c18.7 0 36.2 9.4 46.6 24.9zM80 128V432c0 17.7 14.3 32 32 32H336c17.7 0 32-14.3 32-32V128H80zm80 64V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16z"/></svg>
+        </div>
+      `;
+      listCarts.appendChild(newLi);
     }
   });
+  total.innerHTML = totalPrice.toLocaleString() + ' VND';
+  const quantity = document.querySelector('.header-row-quantity');
+  quantity.innerHTML = count;
 }
-loadItemProduct();
+function changeQuantity(index, newQuantity) {
+  if (newQuantity === 0) {
+    const result = confirm(
+      ` Bạn có chắc chắn muốn xoá ${listCart[index].name} `
+    );
+    result && delete listCart[index];
+  } else {
+    listCart[index].quantity = newQuantity;
+    listCart[index].price = newQuantity * products[index].price;
+  }
+  renderUI();
+}
+function deleteCartItem(index) {
+  const result = confirm(` Bạn có chắc chắn muốn xoá ${listCart[index].name} `);
+  result && delete listCart[index];
+  renderUI();
+}
+const cart = document.querySelector('.cart');
+const body = document.querySelector('body');
+const closeShopping = document.querySelector('.closeShopping');
+function toggleCart() {
+  const cartIcon = document.querySelector('.header-row-icon');
+  cartIcon.addEventListener('click', () => {
+    body.classList.toggle('active');
+    cart.classList.toggle('active');
+  });
+}
+function closeCart() {
+  closeShopping.addEventListener('click', () => {
+    body.classList.remove('active');
+    cart.classList.remove('active');
+  });
+}
+function Start() {
+  closeCart();
+  loadItemProduct();
+  toggleCart();
+}
+Start();
